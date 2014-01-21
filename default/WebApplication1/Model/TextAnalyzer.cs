a01bf0801af526ee893e41ccbcc45807cc9ae827
Grenmyr <dg222cs@student.lnu.se>
2014-01-21 09:45:20 +0100

    pulat om

--- default/WebApplication1/Model/TextAnalyzer.cs
+++ default/WebApplication1/Model/TextAnalyzer.cs
@@ -9,16 +9,14 @@ namespace Versaler.Model
     public static class TextAnalyzer
     {
         public static int GetNumberOfCapitals(this string text){
-
-            
-            int myCount;
-            
-            Regex myRegex = new Regex("[A-ZÅÄÖ]");
-            myCount = myRegex.Matches(text).Count;
-
-            
-            
-            return 5;
+            int myCount = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i])) {
+                    myCount++;
+                }
+            }
+            return myCount;
         }
     }
 }
